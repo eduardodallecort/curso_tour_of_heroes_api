da52b2bd1e1a140af550285b54d1ee25ab42bf0a
Eduardo Dalle Cort <dallecorteduardo@gmail.com>
2021-07-28 18:04:47 -0300

    Adiciona namespace Api e ordena heroes

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -1,16 +1,19 @@
-class HeroesController < ApplicationController
+class Api::HeroesController < ApplicationController
   before_action :set_hero, only: [:show, :update, :destroy]
 
   # GET /heroes
   def index
-    @heroes = Hero.all
-
+    @heroes = Hero.all.sorted_by_name
+    # @heroes = Hero.all
     render json: @heroes
   end
 
   # GET /heroes/1
   def show
     render json: @hero
+    # render json: {
+    #   id: @hero.id, name: @hero.name
+    # }
   end
 
   # POST /heroes
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,6 @@
 class Hero < ApplicationRecord
     validates :name, presence: true
+
+    scope :sorted_by_name, -> { order(:name)}
+    scope :sorted_by_name_desc, -> { order(name: :desc)}
 end
